da6a6346cd205070c2ad211b3405dabe00c043eb
Mafusn <Magnus.peetz.holt@gmail.com>
2022-09-27 15:03:38 +0200

    ingen ide

--- src/session4/B_Multiple_Inheritance/SportsCar.java
+++ src/session4/B_Multiple_Inheritance/SportsCar.java
@@ -3,5 +3,4 @@ package session4.B_Multiple_Inheritance;
 public abstract class SportsCar implements Vehicle {
 
     public abstract boolean goFast();
-
 }
--- src/session4_exercises/problem5/Fridge.java
+++ src/session4_exercises/problem5/Fridge.java
@@ -1,5 +1,7 @@
 package session4_exercises.problem5;
 
+import java.util.Arrays;
+
 public class Fridge {
     private int temperature;
     private Item[] items;
@@ -16,7 +18,9 @@ public class Fridge {
     public void getItem() {
         int length = this.items.length;
         for (int i = 0; i < length && this.items[i] != null; i++) {
-            System.out.println(this.items[i]);
+            System.out.println(Arrays.toString(new Item[]{this.items[i]}));
         }
+        System.out.println(this.items.length);
+        System.out.println(Arrays.toString(new Item[]{this.items[1]}));
     }
 }
